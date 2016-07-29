d89d203e189c7022081b07570ede8adafae33d09
Zac Knaus <Zachary.Knaus@gmail.com>
2016-07-28 20:41:30 -0400

    First Draft

--- datacollect.py
+++ datacollect.py
@@ -1,5 +1,5 @@
 # import html accessability
-import urllib2
+import urllib2, json
 
 # define static variables
 staticUrl = 'https://api.meetup.com/2/events?key=1074395949437b7e6522f50657f5d2a&group_urlname=DCPython'
@@ -7,9 +7,20 @@ print(staticUrl)
 
 # request API data
 apiData = urllib2.urlopen(staticUrl).read()
-print(apiData)
+#print(apiData)
 
 # parse the api data to populate needed variables
+content = json.loads(apiData)
+results = content['results']
+resultCount = 0
+# count number of events
+for i in results:
+    resultCount += 1
+    if resultCount < 5:
+        eventName = i['name']
+        yesRsvp = i['yes_rsvp_count']
+        print(eventName + ' - ' + str(yesRsvp))
+
 
 # variable results
 
