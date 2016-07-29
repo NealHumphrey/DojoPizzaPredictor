add31bf777b62aa5bcd36327a8f3ed0f2b14a5d9
Zac Knaus <Zachary.Knaus@gmail.com>
2016-07-28 20:50:18 -0400

    In a Function

--- datacollect.py
+++ datacollect.py
@@ -2,28 +2,34 @@
 import urllib2, json
 
 # define static variables
-staticUrl = 'https://api.meetup.com/2/events?key=1074395949437b7e6522f50657f5d2a&group_urlname=DCPython'
-print(staticUrl)
-
-# request API data
-apiData = urllib2.urlopen(staticUrl).read()
-#print(apiData)
-
-# parse the api data to populate needed variables
-content = json.loads(apiData)
-results = content['results']
-resultCount = 0
-# count number of events
-for i in results:
-    resultCount += 1
-    if resultCount < 5:
-        eventName = i['name']
-        yesRsvp = i['yes_rsvp_count']
-        print(eventName + ' - ' + str(yesRsvp))
+groupName = raw_input('What is the name of the group you wish to retrieve events for?')
+eventCount = raw_input('How many events do you want to check?')
 
 
-# variable results
 
-# call neal's program and pass the collected variable results
+def getApiData(groupName, eventCount):
+    try:
+        eventCount = int(eventCount)
+    except:
+        print('You entered an invalid number')
+    #staticUrl = 'https://api.meetup.com/2/events?key=1074395949437b7e6522f50657f5d2a&group_urlname=DCPython'
+    dynUrl = 'https://api.meetup.com/2/events?key=1074395949437b7e6522f50657f5d2a&group_urlname=' + groupName
+
+    # request API data
+    apiData = urllib2.urlopen(dynUrl).read()
+
+    # parse the api data to populate needed variables
+    content = json.loads(apiData)
+    results = content['results']
+    resultCount = 0
+
+    # count number of events
+    for i in results:
+        resultCount += 1
+        if resultCount < eventCount:
+            eventName = i['name']
+            yesRsvp = i['yes_rsvp_count']
+            print(eventName + ' - ' + str(yesRsvp))
 
 
+getApiData(groupName, eventCount)
