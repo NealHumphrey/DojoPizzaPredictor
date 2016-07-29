4df52f2599fd89e696c1c9800428eb8753a842de
NealHumphrey <humphrey.neal@gmail.com>
2016-07-28 21:16:41 -0400

    working ish version

--- bottletest.py
+++ bottletest.py
@@ -1,5 +1,10 @@
-from bottle import route, run, template
+from bottle import route, run, template, view
+from main import how_many
 
+#assume 25 people
+view_stuff = how_many(25) 
+
+print(view_stuff)
 
 @route('/hello')
 def hello():
@@ -7,8 +12,8 @@ def hello():
 
 
 @route('/')
+@view('main_template')
 def main():
-	return template('main_template', pizzas = 24)
-
+	return template('main_template', pizzas = 6)
 
 run(host='localhost', port=8080, debug=True)
